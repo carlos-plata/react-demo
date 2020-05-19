0aefea54a81c000bd385705ba472fd45a52912c4
Carlos Plata <44138358+carlos-plata@users.noreply.github.com>
2020-05-19 18:13:56 -0500

    added unordered list

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// JSX
 ReactDOM.render(
-  <div>
-    <h1>Hello world!</h1>
-    <p>This is a paragraph</p>
-  </div>,
+  <ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+  </ul>,
   document.getElementById("root")
 );
-
-var myNewP = document.createElement("p");
-myNewP.innerHTML = "This is a paragraph.";
