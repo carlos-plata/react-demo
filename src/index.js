40f62ec55883dca676f618169f9a7ce755403968
Carlos Plata <44138358+carlos-plata@users.noreply.github.com>
2020-05-19 18:25:09 -0500

    added functional component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// JSX
-ReactDOM.render(
-  <div>
-    <h1>Hello world!</h1>
-    <p>This is a paragraph</p>
-  </div>,
-  document.getElementById("root")
-);
+function MyApp() {
+  return (
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+    </ul>
+  );
+}
 
-var myNewP = document.createElement("p");
-myNewP.innerHTML = "This is a paragraph.";
+ReactDOM.render(<MyApp />, document.getElementById("root"));
